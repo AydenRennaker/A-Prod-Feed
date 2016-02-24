dcc1ef96daf60f83904b1e4175d6a70a3f8521cc
atanner <ayden.rennaker@gmail.com>
2016-02-23 22:19:21 -0700

    Fixed Function

--- query.php
+++ query.php
@@ -1,10 +1,11 @@
-<?
+<?php
 	
 	include "inc/request_generator.php";
+	include "settings/config.php";
 	
 	$params = array( "ItemPage" => "2" );
 	
-	$url = getXMLLink;
+	$url = getXMLLink($params);
 	
 	echo $url;
 
